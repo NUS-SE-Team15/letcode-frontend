513d2e66ba1bfc4fbe8bf5cca0f1f054e5668030
Agelessbaby <205902584@qq.com>
2024-10-24 16:19:14 +0800

    add about us

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -114,13 +114,13 @@ export const routes: Array<RouteRecordRaw> = [
   //     access: ACCESS_ENUM.ADMIN,
   //   },
   // },
-  // {
-  //   path: "/about",
-  //   name: "关于我的",
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () =>
-  //     import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  // },
+  {
+    path: "/about",
+    name: "About Us",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
 ];
---
+++ src/views/AboutView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="about-me">
+    <h1>About Us</h1>
+    <p>{{ authorInfo.name }}</p>
+    <p>{{ authorInfo.us }} years old</p>
+    <p>{{ authorInfo.bio }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AboutMe",
+  data() {
+    return {
+      authorInfo: {
+        projname: "LetCode",
+        us: "Nus Iss SE Team15",
+        bio: "A passionate dev team having interest in Software Engineering",
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+.about-me {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  padding: 20px;
+}
+
+h1 {
+  color: #333;
+}
+
+p {
+  color: #666;
+  font-size: 16px;
+}
+</style>
