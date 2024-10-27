a67ef704546df01bc74ba3eba2b860e80dc45002
whqwq <1776678487@qq.com>
2024-10-27 17:23:41 +0800

    change backend url to tecent cloud

--- generated/core/OpenAPI.ts
+++ generated/core/OpenAPI.ts
@@ -21,8 +21,9 @@ export type OpenAPIConfig = {
 export const OpenAPI: OpenAPIConfig = {
   // 在开发环境下，BASE 为 /api，通过 Vue CLI 代理；在生产环境下，使用实际的后端服务器地址
   //BASE: "http://LetCodeLB-457706603.ap-southeast-1.elb.amazonaws.com:8101",
-  BASE: process.env.NODE_ENV === 'development' ? '/api' : 'http://LetCodeLB-457706603.ap-southeast-1.elb.amazonaws.com:8101',
+  // BASE: process.env.NODE_ENV === 'development' ? '/api' : 'http://LetCodeLB-457706603.ap-southeast-1.elb.amazonaws.com:8101',
   //BASE: "http://127.0.0.1:8101",
+  BASE: process.env.NODE_ENV === 'development' ? '/api' : 'http://43.134.114.17:8101',
   VERSION: "1.0",
   WITH_CREDENTIALS: true, // 允许携带 Cookie 和其他凭证
   CREDENTIALS: "include",
